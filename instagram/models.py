b4ccc1b3af912535d55f34b8796d8739856326e0
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-03-17 15:00:58 +0900

    json 직렬화

---
+++ instagram/models.py
@@ -0,0 +1,10 @@
+from tkinter import CASCADE
+from django.conf import settings
+from django.db import models
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
